04e8a725163ea0c1866cbd9af57819a180ea5863
OVOSHGIF2 <artemzhigulev1603@gmail.com>
2025-05-24 02:45:47 +0400

    Добавил звуки монеток,Добавил охранника и киллзону

--- items/gold.gd
+++ items/gold.gd
@@ -8,4 +8,4 @@ func _on_body_entered(body: Node2D) -> void:
 		tween.tween_property(self, "position", position -  Vector2(0, 25), 0.3)
 		tween1.tween_property(self, "modulate:a", 0, 0.3)
 		tween.tween_callback(queue_free)
-		
+		$"PickUp Sound".play()
